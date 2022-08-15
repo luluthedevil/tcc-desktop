8fab5c85d6b3381c2b6bcab13f49d28e1208fca8
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-08-15 11:03:47 -0300

    Creating entities

--- backend/progress/dto/update-progress.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProgressDto } from './create-progress.dto';
-
-export class UpdateProgressDto extends PartialType(CreateProgressDto) {}
--- backend/progress/progress.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProgressService } from './progress.service';
-import { ProgressController } from './progress.controller';
-
-@Module({
-  controllers: [ProgressController],
-  providers: [ProgressService]
-})
-export class ProgressModule {}
