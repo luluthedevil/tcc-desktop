7d055acb21cd8cda74f0f2fa557b407c05072dd3
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-09-26 11:28:44 -0300

    Backend challenge service

--- backend/kitab/src/challenges/challenges.service.ts
+++ backend/kitab/src/challenges/challenges.service.ts
@@ -1,26 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { CreateChallengeDto } from './dto/create-challenge.dto';
-import { UpdateChallengeDto } from './dto/update-challenge.dto';
+import { Inject, Injectable } from '@nestjs/common';
+//import { CreateChallengeDto } from './dto/create-challenge.dto';
+//import { UpdateChallengeDto } from './dto/update-challenge.dto';
+import { Repository } from 'typeorm';
+import { Challenge } from './entities/challenge.entity';
 
 @Injectable()
 export class ChallengesService {
-  create(createChallengeDto: CreateChallengeDto) {
-    return 'This action adds a new challenge';
+  constructor( @Inject('Challenge_REPOSITORY')
+  private challengeRepository: Repository<Challenge> ) {}
+
+  create(name: string): Promise<Challenge> {
+    const newChallenge = this.challengeRepository.create({name});
+    return this.challengeRepository.save(newChallenge); // insert or update
   }
 
-  findAll() {
-    return `This action returns all challenges`;
+  async findAll(): Promise<Challenge[]> {
+    return this.challengeRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} challenge`;
+  async findOne(id: number): Promise<Challenge> {
+    try {
+      const challenge = await this.challengeRepository.findOneOrFail({where: {id: id}});
+      return challenge;
+    } catch (err) {
+      throw err;
+    }
   }
 
-  update(id: number, updateChallengeDto: UpdateChallengeDto) {
-    return `This action updates a #${id} challenge`;
+  async update(id: number, name: string): Promise<Challenge> {
+    const challenge = await this.findOne(id);
+    challenge.name = name;
+    return this.challengeRepository.save(challenge); //update
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} challenge`;
+  async remove(id: number): Promise<Challenge> {
+    const challenge = await this.findOne(id);
+    return this.challengeRepository.remove(challenge);
   }
 }
