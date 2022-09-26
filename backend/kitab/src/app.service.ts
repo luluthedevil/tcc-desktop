ffb310201f2d2f5bf88b6fc74d8b4caecc72b1e3
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-09-26 11:16:31 -0300

    Backend user service

--- backend/kitab/src/app.service.ts
+++ backend/kitab/src/app.service.ts
@@ -21,39 +21,10 @@ export class AppService {
 
   }
 
-  getAllUsers(): Promise<User[]> {
-    return this.userRepository.find();
-  }
-
   getAllUsersBooks(): Promise<User[]> {
     return this.userRepository.find({
       relations: ['book']
     }); // select * user join books
-  }
-
-  async getUserById(id: number): Promise<User> {
-    try {
-      const user = await this.userRepository.findOneOrFail({where: {id: id}});
-      return user;
-    } catch (err) {
-      throw err;
-    }
-  }
-
-  createUser(username: string, email: string): Promise<User> {
-    const newUser = this.userRepository.create({username, email});
-    return this.userRepository.save(newUser); // insert or update
-  }
-
-  async updateUser(id: number, username: string): Promise<User> {
-    const user = await this.getUserById(id);
-    user.username = username;
-    return this.userRepository.save(user); //update
-  }
-
-  async deleteUser(id:number): Promise<User> {
-    const user = await this.getUserById(id);
-    return await this.userRepository.remove(user);
   }
 
   customQUery(): any {
--- backend/kitab/src/users/users.service.ts
+++ backend/kitab/src/users/users.service.ts
@@ -1,26 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor( @Inject('User_REPOSITORY')
+  private userRepository: Repository<User> ) {}
+
+  create(username: string, email: string): Promise<User> {
+    const newUser = this.userRepository.create({username, email});
+    return this.userRepository.save(newUser); // insert or update
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number): Promise<User> {
+    try {
+      const user = await this.userRepository.findOneOrFail({where: {id: id}});
+      return user;
+    } catch (err) {
+      throw err;
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, username: string): Promise<User> {
+    const user = await this.findOne(id);
+    user.username = username;
+    return this.userRepository.save(user); //update
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number): Promise<User> {
+    const user = await this.findOne(id);
+    return this.userRepository.remove(user);
   }
 }
