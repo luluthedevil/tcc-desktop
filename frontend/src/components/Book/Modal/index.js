3623aaa63b609111f25daccc1b09b0b1938262b2
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-11-28 09:21:04 -0300

    Frontend - refactoring book modal

--- frontend/src/components/Book/Modal/index.js
+++ frontend/src/components/Book/Modal/index.js
@@ -2,27 +2,30 @@ import React, { useState } from 'react';
 import { GrClose } from 'react-icons/gr';
 import { BsBookmark, BsBookmarkFill } from 'react-icons/bs';
 import './style.css';
+import semCapa from '../../../imgs/capa_para_sem_capa.png';
 import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export default function Modal ({showModal, info, onClose}){
+export default function Modal ({showModal, info, onClose, thumbnail}){
     const [isSaved, setIsSaved] = useState(false);
-    const volumeInfo = info.volumeInfo;
-    const accessInfo = info.accessInfo;
-    const selfLink = info.selfLink;
-    const data = {
-        volumeInfo,
-        accessInfo,
-        selfLink
-    }
-    
-    function handleSave() {
-        //console.log(data)
-        //console.log(process.env.GOOGLE_SHEETS_LINK)
-        axios.post(`https://sheet.best/api/sheets/${process.env.REACT_APP_GOOGLE_SHEET_LINK}`, data).then((response) => {
-            console.log(response.status)
+    const baseURL = 'http://localhost:3333/';
+    async function postMongo() {
+        axios.post(`${baseURL}books`, {
+            "image": info.imageLinks ? info.imageLinks.smallThumbnail : semCapa,
+            "title": info.title,
+            "author": JSON.stringify(info.authors),
+            "publisher": info.publisher,
+            "publishedDate": info.publishedDate,
+            "previewLink": info.previewLink,
+            "description": info.description
         })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     }
     if(!showModal) {
         return null;
@@ -32,7 +35,7 @@ export default function Modal ({showModal, info, onClose}){
             <div className="overlay-inner">
                 <div className="modal-buttons">
                     <div>
-                        <button type="submit" className="save-btn" onClick={() => {setIsSaved(!isSaved); handleSave();}}>
+                        <button id="save-book" type="submit" className="save-btn" onClick={() => {setIsSaved(!isSaved); postMongo();}}>
                             {isSaved ? 
                             <BsBookmarkFill />
                             : 
@@ -40,34 +43,30 @@ export default function Modal ({showModal, info, onClose}){
                         </button>
                     </div>
                     <div>
-                        <button className="close-btn" onClick={() => onClose}>
+                        <button className="close-btn" onClick={() => onClose()}>
                             <GrClose />
                         </button>
                     </div>
                 </div>
                 <div className="inner-box">
-                    <img alt="book-cover" src={
-                        info.volumeInfo.imageLinks 
-                        && 
-                        info.volumeInfo.imageLinks.smallThumbnail
-                        }  />
+                    <img alt="book-cover" src={thumbnail} />
                     <div className="book-info">
-                        <h1 className="book-title">{info.volumeInfo.title}</h1>
-                        <h3 className="book-author-name">{info.volumeInfo.authors + ''}</h3>
+                        <h1 className="book-title">{info.title}</h1>
+                        <h3 className="book-author-name">{info.authors + ''}</h3>
                         <h4>
-                            {info.volumeInfo.publisher + " ⋆ "}
+                            {info.publisher + " ⋆ "}
                             <span>
-                                {info.volumeInfo.publishedDate}
+                                {info.publishedDate}
                             </span>
                         </h4>
-                        <a href={info.volumeInfo.previewLink}>
+                        <a href={info.previewLink}>
                             <button className="more-btn">
                                 More
                             </button>
                         </a>
                     </div>
                 </div>
-                {info.volumeInfo.description ? <p className="book-description">{info.volumeInfo.description}</p> : ''}
+                {info.description ? <p className="book-description">{info.description}</p> : ''}
             </div>
         </div>
     );
