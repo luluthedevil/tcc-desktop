c1c4be0d5cd74791e473cecdeda45ee7e58abab9
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2022-11-28 09:21:20 -0300

    Frontend - refactoring book list

--- frontend/src/components/BookList/index.js
+++ frontend/src/components/BookList/index.js
@@ -1,19 +1,13 @@
 import React, {useRef} from 'react';
 import Book from '../Book';
 import './style.css';
-import { useNavigate } from 'react-router-dom';
 import { IoIosArrowDropleft, IoIosArrowDropright } from 'react-icons/io';
 
-export default function Booklist({title, livros, isLibrary = false}) {
-  const navigate = useNavigate();  
-  const goToBookDetails = (book) => {
-    let path = `/book-details`; 
-    navigate(path, {state: {book: book}});
-  };
-  const handleBook = (book, index, isLibrary) => (
-    <div className="item" key={index} onClick={() => goToBookDetails(book)}>
-      <Book key={book.volumeInfo.title} info={book}
-      sheet={isLibrary}
+export default function Booklist({title, livros}) { 
+  const handleBook = (book, index) => (
+    <div className="item" key={index}>
+      <Book key={book.title} info={book}
+      thumbnail={book.image}
       />
     </div>
   );
@@ -40,7 +34,7 @@ export default function Booklist({title, livros, isLibrary = false}) {
           <div className="books carousel" ref={carousel}>
             {livros.map((book, index) => (
               // console.log(book)
-              handleBook(book, index, isLibrary)
+              handleBook(book, index)
             ))}
           </div>
           <button className="button" onClick={handleClickRight}>
