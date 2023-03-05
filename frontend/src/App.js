e5345844a06ac68ef7bf7143cbd3c00760a9f9a2
luluthedevil <luciana.miranda@academico.ifpb.edu.br>
2023-03-05 14:26:52 -0300

    Frontend - organizing app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,19 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/nav';
-import HomeScreen from './screens/HomeScreen/HomeScreen';
-import BooksScreen from './screens/BooksScreen/BooksScreen';
-import LibraryScreen from './screens/LibraryScreen/LibraryScreen';
-import PoliticsScreen from './screens/PoliticsScreen/PoliticsScreen';
-import RewardsScreen from './screens/RewardsScreen/RewardsScreen';
-import TermsScreen from './screens/TermsScreen/TermsScreen';
+import RoutesApp from "./routes";
 import Warn from './components/warn';
-import BookDetailsScreen from './screens/BookDetailsScreen/BookDetailsScreen';
-import BookListingScreen from './screens/BookListingScreen/BookListingScreen';
-require('dotenv').config();
+
+import { AuthProvider } from "./contexts/auth";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <div className="container">
-          <Warn />
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/books" element={<BooksScreen />} />
-            <Route path="/library" element={<LibraryScreen />} />
-            <Route path="/politcs" element={<PoliticsScreen />} />
-            <Route path="/rewards" element={<RewardsScreen />} />
-            <Route path="/terms" element={<TermsScreen />} />
-            <Route path="/book-details" element={<BookDetailsScreen />} />
-            <Route path="library/livros-lidos" element={<BookListingScreen />} />
-            <Route path="library/livros-a-ler" element={<BookListingScreen />} />
-          </Routes>
-        </div>
+    <AuthProvider>
+    <div className="App">
+      <RoutesApp />
+      <div className="container">
+        <Warn />
       </div>
-    </Router>
+    </div>
+  </AuthProvider>
   );
 }
 
